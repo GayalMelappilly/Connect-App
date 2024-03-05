9d0374a792d41777e81d79616ec7118e175d6095
Gayal Melappilly <gayalsunil@gmail.com>
2024-03-06 00:16:51 +0530

    Title added

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var hbs = require('express-handlebars')
+var db = require('./config/connection')
 
-var indexRouter = require('./routes/index');
+var adminRouter = require('./routes/admin');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -13,14 +15,29 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.engine('hbs',
+  hbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + '/views/layout/',
+    partialsDir: __dirname + '/views/partials/',
+    handlebars: require('handlebars'), // Ensure you have Handlebars as a dependency
+    allowProtoMethodsByDefault: true // Disable the prototype access check
+  }))
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+db.connect((err)=>{
+  if(err) console.log(err)
+  else console.log('DATABASE CONNECT SUCCESSFULLY')
+})
+
+app.use('/admin', adminRouter);
+app.use('/', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
