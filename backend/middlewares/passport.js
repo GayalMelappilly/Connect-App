3b95e600b1b9e391680e284692fcec0a6275b38d
Gayal Melappilly <gayalsunil@gmail.com>
2024-05-21 03:24:51 +0530

    Almost done

--- backend/middlewares/passport.js
+++ backend/middlewares/passport.js
@@ -9,7 +9,7 @@ passport.use(
     new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: 'http://localhost:5000/auth/google/callback',
+        callbackURL: 'https://connect-app-ykav.onrender.com/auth/google/callback',
         scope: ['profile', 'email'],
         prompt: 'select_account'
     },
